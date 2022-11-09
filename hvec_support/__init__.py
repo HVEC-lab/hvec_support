24c1450ecd31847429908e5a774a55537a20c303
Hessel Voortman <hessel@hesselvoortman.nl>
2022-11-09 15:39:13 +0100

    Modified package structure

--- hvec_support/__init__.py
+++ hvec_support/__init__.py
@@ -8,10 +8,3 @@ from .admin import __author__, __author_email__, __version__
 from . import excel
 from . import sqlite
 from . import styles
-
-from .bulk_importers import ipcc
-from .bulk_importers import psmsl
-from .bulk_importers import rws
-from .bulk_importers import knmi
-from .bulk_importers import gloss
-from .bulk_importers import data_handling
